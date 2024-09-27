07c92fcdb9dd79fe3551bb4ea6f69a91f11ce97a
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-27 10:53:48 -0700

    dvsdv

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,34 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary", 3));
+    System.out.println(DelEnd("library", 5));
   }
 
-  // write solutions to problems below
+  public static String firstLastN(String word, int n)
+  {
+    // word = dictionary
+    // n = 3 
+    // dicary
+    String output = newString();
 
+    // Getting the first n letters
+    String firstN = word.substring(0,n); 
+
+    // Geting the last n letters
+    int indexOfNToLast = word.length() - n; // index of the nth to last letter
+    String lastN = word.substring(indexOfNToLast); // last n letters, starting at nth to last letter 
+
+    output = firstN + LastN;
+    return output;
+  }
+}
+
+public static String DelEnd(String word, int n)
+}
+{
+  String output = new String();
+  int wordlength = word.length();
+  String output = word.substring(0, wordlength - n);
+  return output;
 }
